29d21fcb559b0a1009f1323ac6f89694b0ff2ea5
Bhupendra Gautam <Bhupendragtm777@gmail.com>
2022-01-14 11:56:35 +0000

    Update Task after DTO step

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,21 +1,19 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
-import { TasksService } from './tasks.service';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
 import { Task } from './task.model';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  async getAllTasks(): Promise<Task[]> {
+  getAllTasks(): Task[] {
     return this.tasksService.getAllTasks();
   }
 
   @Post()
-  async createTask(
-    @Body('title') title: string,
-    @Body('description') description: string,
-  ): Promise<Task> {
-    return this.tasksService.createTask(title, description);
+  createTask(@Body() createTaskDto: CreateTaskDto): Task {
+    return this.tasksService.createTask(createTaskDto);
   }
 }
