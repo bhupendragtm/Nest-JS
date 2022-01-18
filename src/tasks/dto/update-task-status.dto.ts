087a0a7edba7dc13eec8c3fd1a8b3c69b9a57629
Bhupendra Gautam <Bhupendragtm777@gmail.com>
2022-01-18 16:23:38 +0000

    Type ORM Started

--- src/tasks/dto/update-task-status.dto.ts
+++ src/tasks/dto/update-task-status.dto.ts
@@ -1,7 +1,7 @@
-import { IsEnum } from 'class-validator';
+// import { IsEnum } from 'class-validator';
 import { TasksStatus } from '../task-status.enum';
 
 export class UpdateTaskStatusDto {
-  @IsEnum(TasksStatus)
+  // @IsEnum(TasksStatus)
   status: TasksStatus;
 }
